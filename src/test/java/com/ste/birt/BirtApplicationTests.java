a0e5aeab63b9cfa4842d39eb8951aba4e7bdfccc
thiri <thiri.haymar@stagg.com>
2020-08-16 22:34:20 +0800

    demo birt first commit

---
+++ src/test/java/com/ste/birt/BirtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ste.birt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
